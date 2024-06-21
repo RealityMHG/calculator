ad90637256ea6221a66b4902fec08cd5fad32e88
RealityMHG <rafaelrego2000@gmail.com>
2024-06-21 13:22:53 +0100

    plus and minus operation work with basic tests, equals works differently according to normal calculators

--- script.js
+++ script.js
@@ -2,18 +2,67 @@
 let screenDisplay = document.querySelector(".screen-text");
 let screenArray = screenDisplay.textContent.split();
 
+//Variables for the operations
+let numbersForOp = [];
+let currentOp = "empty";
+let hasEnteredAnything = false;
+
 //Adding event listeners to the buttons
 //Numbers
 let allNumberBtns = document.querySelectorAll(".number-btn");
 allNumberBtns.forEach((number) => {
     number.addEventListener("click", () => {
-        if(number.textContent == "del") {
-            screenArray.pop();
-            screenDisplay.textContent = screenArray.join("");
-        } else {
-            screenArray.push(number.textContent);
-            screenDisplay.textContent = screenArray.join("");
-         }
+        switch(number.textContent) {
+            case "del":
+                if(screenArray.length == 1) {
+                    resetScreen();
+                } else {
+                    screenArray.pop();
+                    updateScreen();
+                }               
+                break;
+            default:
+                if(!hasEnteredAnything) {
+                    screenArray = [];
+                    hasEnteredAnything = true;
+                }
+                screenArray.push(number.textContent);
+                updateScreen();
+                break;
+        }
+    });
+});
+
+//Operations
+let allOpBtns = document.querySelectorAll(".op-btn");
+allOpBtns.forEach((op) => {
+    op.addEventListener("click", () => {
+        switch(op.textContent) {              
+            case "=":
+                numbersForOp.push(Number(getNumberOnScreen()));
+                let result = getOperationResult(numbersForOp,currentOp);
+                screenArray = [result];
+                updateScreen();
+                numbersForOp = [];
+                currentOp = "empty";
+                hasEnteredAnything = false;
+                break;
+
+            default:
+                numbersForOp.push(Number(getNumberOnScreen()));
+                if(currentOp == "empty") {
+                    currentOp = op.textContent;
+                    resetScreen();
+                } else {
+                    let result = getOperationResult(numbersForOp,currentOp);
+                    screenArray = [result];
+                    updateScreen();
+                    numbersForOp = [result];
+                    hasEnteredAnything = false;
+                }
+                break;
+        }
+    
     });
 });
 
@@ -21,6 +70,30 @@ allNumberBtns.forEach((number) => {
 
 
 
-
-
 //Functions
+function getOperationResult (numberArray, operation) {
+    let operationResult = 0;
+    switch(operation) {
+        case "+":
+            operationResult = numberArray.reduce((total, num) => (total+=num));
+            break;
+        case "-":
+            operationResult = numberArray.reduce((total,num) => (total-=num));
+            break;
+    }
+    return operationResult;
+}
+
+function getNumberOnScreen() {
+    return screenArray.join("");
+}
+
+function updateScreen() {
+    screenDisplay.textContent = getNumberOnScreen();
+}
+
+function resetScreen() {
+    screenArray = [0];
+    hasEnteredAnything = false;
+    updateScreen();
+}
